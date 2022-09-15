e0e8a22dfa5582d7b5afd8f6d49224f97e81fb6b
Haofphm4426 <haofphm4426@gmail.com>
2022-09-15 18:02:12 +0700

    structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>ReactJs</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, idx) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={idx}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
